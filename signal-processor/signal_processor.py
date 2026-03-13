7a794db67f8def116145f5ce1af384a8c51bad36
unknown <mpp.38544@gmail.com>
2026-03-13 22:37:40 +0800

    feature: added signal processor with Kalman Filter + Avellaneda-Stoikov model

---
+++ signal-processor/signal_processor.py
@@ -0,0 +1,169 @@
+import numpy as np
+from confluent_kafka import Consumer
+from confluent_kafka import Producer
+import json
+from datetime import datetime
+
+
+class KalmanFilter:
+    def __init__(self):
+        self.F = np.array([[1, 1],
+                           [0, 1]])
+        
+        self.H = np.array([[1, 0]])
+
+        self.Q = np.array([[1e-4, 0],
+                           [0, 1e-4]])
+        
+        self.R = np.array([[0.1]])
+
+        self.P =  np.eye(2)
+
+        self.x  = np.array([[0.0], 
+                            [0.0]])
+
+        self.I = np.eye(2)
+
+    def update(self, obs):
+        self.x = self.F @ self.x
+
+        self.P = self.F @ self.P @ self.F.T + self.Q
+
+        self.K = self.P @ self.H.T @ np.linalg.inv(self.H @ self.P @ self.H.T + self.R)
+
+        self.x = self.x + self.K @ (obs - self.H @ self.x)
+
+        self.P = (self.I - self.K @ self.H) @ self.P
+
+        return self.x
+
+class AvellanedaStoikov:
+    def __init__(self):
+        self.gamma = 0.1
+        self.kappa = 1.5
+        self.max_pos = 0.01
+        self.order_size = 0.001
+
+    def calculate_quotes(self, fair_price, sigma, inv, t_left):
+
+        sigma_sq = sigma ** 2
+        inv_skew = inv * self.gamma * sigma_sq * t_left
+
+        inv_skew = np.clip(inv_skew, -fair_price * 0.01, fair_price * 0.01)
+
+        reserv_price = fair_price - inv_skew
+
+        spread = (self.gamma * sigma_sq * t_left) + (2/self.gamma) * np.log(1 + self.gamma / self.kappa)
+
+        spread = np.clip(spread, fair_price * 0.0002, np.inf)
+
+        bid = reserv_price - spread / 2
+
+        ask = reserv_price + spread / 2
+
+        return [bid, ask]
+
+class SignalProcessor:
+    def __init__(self):
+        self.kF = KalmanFilter()
+        self.aS = AvellanedaStoikov()
+        
+        self.kafka_consumer = Consumer({"bootstrap.servers" : "localhost:9092", "group.id": "signal-processor", "auto.offset.reset": "earliest"})
+
+        self.kafka_consumer.subscribe(["raw-ticks"])
+
+        self.kafka_producer = Producer({"bootstrap.servers" : "localhost:9092"})
+
+        self.inv = 0
+
+        self.decay = 0.94
+
+        self.lastPrice = 0.0
+        self.currentVol = 1e-4
+
+
+    def calc_vol(self, price):
+        if self.lastPrice <= 0.0:
+            self.lastPrice = price
+            return np.sqrt(self.currentVol)
+        
+        logReturn = np.log(price / self.lastPrice)
+
+        self.currentVol = (self.decay * self.currentVol) + ((1.0 - self.decay) * (logReturn ** 2))
+
+        self.lastPrice = price
+
+        return np.sqrt(self.currentVol)
+
+    def process_tick(self, tick):
+        '''{
+        "symbol": trade.symbol, 
+        "price": trade.price, 
+        "size": trade.size, 
+        "timestamp": str(trade.timestamp)
+        }'''
+
+        symbol = tick["symbol"]
+        price = tick["price"]
+        size = tick["size"]
+        timestamp = tick["timestamp"]
+
+        obs = np.array([[price]])
+
+        state = self.kF.update(obs)
+
+        fair_price = float(state[0][0])
+
+        sigma = self.calc_vol(fair_price)
+
+        midnight = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+        t_left = 1 - (datetime.now() - midnight).total_seconds() / 86400
+
+        bid, ask = self.aS.calculate_quotes(fair_price, sigma, self.inv, t_left)
+
+        bid = float(bid)
+        ask = float(ask) 
+
+        signals = []
+
+        if price <= bid and self.inv < self.aS.max_pos:
+            trade_sig = {"symbol": symbol, "signal": "BUY", "price": price, "val": bid, "fair_price": fair_price, "inventory": self.inv, "timestamp" : timestamp}
+
+            signals.append(trade_sig)
+
+        if price >= ask and self.inv > -self.aS.max_pos:
+            trade_sig = {"symbol": symbol, "signal": "SELL", "price": price, "val": ask, "fair_price": fair_price, "inventory": self.inv, "timestamp" : timestamp}
+
+            signals.append(trade_sig)
+
+        for sig in signals:
+            self.kafka_producer.produce(topic="trading-signals", value=json.dumps(sig).encode("utf-8"))
+        
+        self.kafka_producer.flush()
+
+    def run(self):
+        while True:
+            msg = self.kafka_consumer.poll(1.0)
+
+            if msg is None:
+                continue
+
+            if msg.error():
+                print(f"Consumer error: {msg.error()}")
+                continue
+
+            try:
+                tick = json.loads(msg.value().decode("utf-8"))
+                self.process_tick(tick)
+
+            except Exception as e:
+                print(f"Error processing tick: {e}")
+                continue
+
+if __name__ == "__main__":
+    processor = SignalProcessor()
+    processor.run()
+
+
+
