2b63156984fbc8d88284af651faf4bdf9bc37480
unknown <mpp.38544@gmail.com>
2026-03-15 00:51:51 +0800

    feature: Added Prometheus instrumentation and Grafana monitoring

--- producer/producer.py
+++ producer/producer.py
@@ -4,10 +4,14 @@ from dotenv import load_dotenv
 import os
 from confluent_kafka import Producer
 import json
+import psycopg2
+from datetime import datetime, timezone
+from prometheus_client import Counter, start_http_server
 
 load_dotenv()
 
 class TradingProducer:
+    ticks_received = Counter('ticks_received_total', 'Total ticks received')
 
     def __init__(self):
         self.ALPACA_API_KEY = os.environ.get("ALPACA_API_KEY")
@@ -15,9 +19,33 @@ class TradingProducer:
 
         self.data_stream = CryptoDataStream(api_key = self.ALPACA_API_KEY, secret_key=self.ALPACA_SECRET_KEY)
 
-        self.symbols = ["BTC/USD", "ETH/USD", "XRP/USD", "AVAX/USD", "LINK/USD"]
+        self.symbols = [
+            "BTC/USD", "ETH/USD", "XRP/USD", 
+            "AVAX/USD", "LINK/USD", "LTC/USD",
+            "BCH/USD", "UNI/USD", "AAVE/USD", "DOT/USD"
+        ]
 
         self.kafka_producer = Producer({"bootstrap.servers" : os.environ.get("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")})
+
+        self.conn = psycopg2.connect(
+            host=os.environ.get("POSTGRES_HOST", "localhost"),
+            port=int(os.environ.get("POSTGRES_PORT", "5433")),
+            database="tradingdb",
+            user="trader",
+            password="trader123"
+        )
+
+        self.cursor = self.conn.cursor()
+
+        start_http_server(8001)
+
+    def write_log(self, symbol, price, qty):
+        self.cursor.execute(
+            "INSERT INTO logs (service, message, timestamp) VALUES (%s, %s, %s)",
+            ("producer", f"Trade Received: {symbol} @ ${price}, Qty: {qty}", datetime.now(timezone.utc))
+        )
+
+        self.conn.commit()
 
 
     async def trade_callback(self, trade):
@@ -29,7 +57,8 @@ class TradingProducer:
         self.kafka_producer.produce(topic="raw-ticks", value=kafka_val)
         self.kafka_producer.flush()
 
-
+        self.write_log(trade.symbol, trade.price, trade.size)
+        self.ticks_received.inc()
 
     def subscribe_trades(self):
         self.data_stream.subscribe_trades(self.trade_callback, *self.symbols)
